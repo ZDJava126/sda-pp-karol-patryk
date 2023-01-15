39bb42555121e662aecd1f3b61256926c5d6cfc8
Karol <karolkopycinski@wp.pl>
2023-01-15 12:14:16 +0100

    Java Class User added-poprawione

--- src/main/java/com/sda/model/User.java
+++ src/main/java/com/sda/model/User.java
@@ -11,7 +11,62 @@ public class User {
    Integer age;
    String email;
 
-
+    public User(String username, String password, String name, String surname, Integer age, String email) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.email = email;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
 
 
