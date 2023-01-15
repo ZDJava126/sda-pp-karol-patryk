579732942b81bf887c78c826bec6f7da3d5fdb70
Karol <karolkopycinski@wp.pl>
2023-01-15 16:24:12 +0100

    changed HibernateUtils&UsersDAOTest

--- src/test/java/com/sda/dao/UsersDAOTest.java
+++ src/test/java/com/sda/dao/UsersDAOTest.java
@@ -1,2 +1,27 @@
-package com.sda.dao;public class UsersDAOTest {
+package com.sda.dao;
+
+import com.sda.model.User;
+import org.junit.jupiter.api.Test;
+
+public class UsersDAOTest {
+
+    private final UsersDAO usersDAO = new UsersDAO();
+
+
+    @Test
+    void testCreateHappyPath() {
+        String expectedUsername = "user1";
+        //given
+        User expectedUser = User.builder()
+                .username(expectedUsername)
+                .password("password")
+                .name("name")
+                .surname("surname")
+                .email("example@gmail.com")
+                .age(30).build();
+        //when
+
+        //then
+
+    }
 }
